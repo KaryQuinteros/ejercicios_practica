3edb1ef28d04a4e59fcd091037b197a823abe5f8
lauzarg <lauzarate@mi.unc.edu.ar>
2025-04-08 22:54:47 -0300

    mis ej de hoy

--- ejercicios_lau.py
+++ ejercicios_lau.py
@@ -1,5 +1,23 @@
 # 1) Hacer un programa donde se pida un nombre por teclado y se imprima “Hola ..el_nombre_ingresado”
 
-nombre = input("ingrese su nombre, por favor ")
+""" nombre = input("ingrese su nombre, por favor ")
 
-print(f'Hola {nombre}' )
+print(f'Hola {nombre}' ) """
+
+#4) Hacer un programa que solicite una edad e imprima “Es mayor” si es mayor de edad , sino que imprima “Es menor”.
+
+""" edad = int(input("ingrese su edad "))
+
+if edad<18:
+    print("usted es menor de edad")
+else:
+    print("usted es mayor") """
+
+def mayoria_edad(Edad):
+    if Edad<18:
+        return 'Es mayor de edad'
+    else:
+        return 'Es menor de edad'
+    
+
+print(mayoria_edad(15))
