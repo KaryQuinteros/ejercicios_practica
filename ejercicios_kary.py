46b798deac5536c597a339ddc9c7fa4320b2f8c2
Karinaquinteros <karinaq38@gmail.com>
2025-04-08 22:53:53 -0300

    ultimo ejercicio funciones

--- ejercicios_kary.py
+++ ejercicios_kary.py
@@ -1,3 +1,23 @@
 
-nombre = input("¿como te llamas?")
-print ("Hola" , nombre)
+#nombre = input("¿como te llamas?")
+#print ("Hola" , nombre)
+
+"""edad = int (input("Ingrese su edad : "))
+if edad >=18:
+    print ("Eres mayor de edad")
+else:
+    print("No eres mayor de edad")    """
+
+edad = int (input("Ingrese su edad : "))
+
+def verificar_edad(edad):
+
+    if edad >=18:
+        return "Eres mayor de edad"
+    else:
+        return "No eres mayor de edad"
+
+print(verificar_edad(edad))
+
+
+
